b77b252370d51471bc14b8e0f217fbd5f32760ed
Ziggy Stardust <zig@Judy-Garland.local>
2019-05-05 12:29:19 -0400

    CarpetCalculator

--- src/Main.java
+++ src/Main.java
@@ -43,15 +43,26 @@ public class Main {
 //        System.out.println("height = " + wall.getHeight());
 //        System.out.println("area = " + wall.getArea());
 
-        Point first = new Point(6,5);
-        Point second = new Point(3,1);
+//        Point first = new Point(6,5);
+//        Point second = new Point(3,1);
+//
+//        System.out.println("dist0,0 = " + first.distance());
+//        System.out.println("dist2nd = " + first.distance(second));
+//        System.out.println("dist22 = " + first.distance(2,2));
+//
+//        Point point = new Point();
+//        System.out.println("dist = " + point.distance());
 
-        System.out.println("dist0,0 = " + first.distance());
-        System.out.println("dist2nd = " + first.distance(second));
-        System.out.println("dist22 = " + first.distance(2,2));
 
-        Point point = new Point();
-        System.out.println("dist = " + point.distance());
+//        Carpet carpet = new Carpet(3.5);
+//        Floor floor = new Floor(2.75, 4.0);
+//        Calculator calculator = new Calculator(floor, carpet);
+//        System.out.println("total = " + calculator.getTotalCost());
+//
+//        carpet = new Carpet(1.5);
+//        floor = new Floor(5.4,4.5);
+//        calculator = new Calculator(floor,carpet);
+//        System.out.println("total = " + calculator.getTotalCost());
 
 
     }
